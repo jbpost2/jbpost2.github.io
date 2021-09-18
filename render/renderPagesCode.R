0cb2dbe73e8b7ec8d218d80aaa93b7e08fca5ee9
Justin Post <jbpost2@ncsu.edu>
2021-09-18 00:28:48 -0400

    See if files are generating correctly

--- render/renderPagesCode.R
+++ render/renderPagesCode.R
@@ -6,3 +6,17 @@ rmarkdown::render("Rmd/README.Rmd",
                   output_file = "../README.md", 
                   output_options = list(html_preview = FALSE)
                   )
+
+#phil
+rmarkdown::render("Rmd/PhilosophyCourses.Rmd", 
+                  output_format = "github_document", 
+                  output_file = "../pages/PhilosophyCourses.md", 
+                  output_options = list(html_preview = FALSE)
+)
+
+#online
+rmarkdown::render("Rmd/Online.Rmd", 
+                  output_format = "github_document", 
+                  output_file = "../pages/Online.md", 
+                  output_options = list(html_preview = FALSE)
+)
